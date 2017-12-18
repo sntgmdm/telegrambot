4f60969e2341e87291bbd64816a94db4ee4b4783
Santiago Medem <santiago.medem@gmail.com>
2017-12-18 23:45:36 +0100

    keep on testing like aperture

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # telegrambot
 Telegram Bot for my Rasberry Pi acting as TorrentBox
 
-This should work as monitoring for the TorrentBox that will be unaccesible. 
+This should work as monitoring for the TorrentBox that will be unaccesible.
 Plus why not some fun with Bots! :)
+
+On my Rasberry, when updating the code I use:
+#git fetch --all
+#git reset --hard origin/master
+
+this way I just have to add the chatbot TOKEN
--- telegrambot.py
+++ telegrambot.py
@@ -7,19 +7,23 @@ from telepot.loop import MessageLoop
 
 def handle(msg):
     chat_id = msg['chat']['id']
+    message_id = msg['id']
+    message_chat = msg['chat']
     command = msg['text']
 
-    print 'Got command: '+ command + ' from: ' + chat_id
+    print 'Got command: '+ command + ' from: ' + str(chat_id) + ' message_id ' + str(message_id) + ' message_chat ' + str(message_id)
 
     if command == '/roll':
         bot.sendMessage(chat_id, random.randint(1,6))
     elif command == '/time':
         bot.sendMessage(chat_id, str(datetime.datetime.now()))
+    else:
+        bot.sendMessage(chat_id, 'I did not understand you')
 
 bot = telepot.Bot('*** INSERT TOKEN ***')
 
 MessageLoop(bot, handle).run_as_thread()
-print 'I am listening ..'
+print 'I am listening ...'
 
 while 1:
     time.sleep(10)
