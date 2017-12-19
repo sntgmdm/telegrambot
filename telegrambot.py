2c06026e1f115bd323d5c70100773448d6e3c9ea
Santiago Medem <santiago.medem@gmail.com>
2017-12-19 02:08:44 +0100

    Added Aziz

--- telegrambot.py
+++ telegrambot.py
@@ -10,6 +10,7 @@ from telepot.loop import MessageLoop
 def handle(msg):
     chat_id = msg['chat']['id']
     command = msg['text']
+    light = false
 
     print 'Got command: '+ command + ' from: ' + str(chat_id)
 
@@ -19,6 +20,13 @@ def handle(msg):
         bot.sendMessage(chat_id, str(datetime.datetime.now()))
     elif command == '/spitip':
         bot.sendMessage(chat_id, str(getip.get()))
+    elif command == '/aziz':
+        if light:
+            bot.sendMessage(chat_id, 'Good boy Aziz')
+            light = !light
+        else:
+            bot.sendMessage(chat_id, 'Aziz light!!!')
+            light = !light
     else:
         bot.sendMessage(chat_id, 'I did not understand you')
 
