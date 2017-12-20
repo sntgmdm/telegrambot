9541ed8582a8f7fbad89c5585c369a302086fe3f
Santiago Medem <santiago.medem@gmail.com>
2017-12-21 00:07:08 +0100

    simplyfing

--- bashquote.py
+++ bashquote.py
@@ -6,14 +6,8 @@ def bashquote( randnum ):
     bash = requests.get('http://bash.org/?' + str(randnum))
     bashtree = html.fromstring(bash.content)
     quote = bashtree.xpath('//p[@class="qt"]/text()')
-    print type(quote)
-    quote_text = list(quotechequer(quote))
-    yield quote_text
-
-def quotechequer ( quote_tbc ):
-    if not quote_tbc:
+    if not quote:
         print "empty quote"
         bashquote(random.randint(7,963184))
     else:
-        print "quote_tbc is of type: " + str(type(quote_tbc))
-        yield quote_tbc
+        yield quote
