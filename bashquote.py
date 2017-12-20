4822482e475270633c38e4e552651bb30ed4d9f8
Santiago Medem <santiago.medem@gmail.com>
2017-12-20 22:49:06 +0100

    to be tested

--- bashquote.py
+++ bashquote.py
@@ -1,8 +1,18 @@
 from lxml import html
 import requests
+import random
+
+random_generation = random.randint(7,8)
+bashquote(random_generation)
 
 def bashquote( randnum ):
     bash = requests.get('http://bash.org/?' + str(randnum))
     bashtree = html.fromstring(bash.content)
-
     quote = bashtree.xpath('//p[@class="qt"]/text()')
+    quotechequer(quote)
+
+def quotechequer ( quote_tbc ):
+    if not quote_tbc:
+        print 'empty quote'
+    else:
+        print quote_tbc
