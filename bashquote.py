938613cf1abc1d4c5533fce4a0fed5d05cddb930
Santiago Medem <santiago.medem@gmail.com>
2017-12-21 00:15:26 +0100

    test with recursion

--- bashquote.py
+++ bashquote.py
@@ -2,12 +2,13 @@ from lxml import html
 import requests
 import random
 
-def bashquote( randnum ):
+def bashquote(randnum):
     bash = requests.get('http://bash.org/?' + str(randnum))
     bashtree = html.fromstring(bash.content)
     quote = bashtree.xpath('//p[@class="qt"]/text()')
     if not quote:
         print "empty quote"
         bashquote(random.randint(7,963184))
+        print "after calling myself"
     else:
         yield quote
