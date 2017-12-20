03c84d8ba901426d6a59acad1e9e5cb50dfde015
Santiago Medem <santiago.medem@gmail.com>
2017-12-20 23:32:02 +0100

    buff

--- bashquote.py
+++ bashquote.py
@@ -7,6 +7,7 @@ def bashquote( randnum ):
     bashtree = html.fromstring(bash.content)
     quote = bashtree.xpath('//p[@class="qt"]/text()')
     quote_text = quotechequer(quote)
+    print type(quote_text)
     return quote_text
 
 def quotechequer ( quote_tbc ):
@@ -14,6 +15,6 @@ def quotechequer ( quote_tbc ):
         print "empty quote"
         bashquote(random.randint(7,963184))
     else:
-        type(quote_tbc)
-        print quote_tbc
+        for a in quote_tbc:
+            print a
         return quote_tbc
