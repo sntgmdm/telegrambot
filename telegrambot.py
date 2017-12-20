0ecf7c7806b57fb064856d65b361f58f109222fe
Santiago Medem <santiago.medem@gmail.com>
2017-12-21 00:20:55 +0100

    finally it worked like a charm

--- telegrambot.py
+++ telegrambot.py
@@ -35,6 +35,7 @@ def handle(msg):
         else:
             bot.sendMessage(chat_id, 'Aziz light!!!')
     elif command == '/bash':
+        bot.sendMessage(chat_id, "Be patient I'm choosing a random quote!")
         quote_tbs = bashquote(random.randint(7,963184))
         for quote_sentence in quote_tbs:
             bot.sendMessage(chat_id, quote_sentence)
